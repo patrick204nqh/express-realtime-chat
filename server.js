bcba05ffc089f3d0823d99c3e3bdb910c2f10d0c
Patrick <patrick204nqh@gmail.com>
2020-05-11 12:15:51 +0700

    direction user to orther room

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,14 +17,20 @@ const botName = 'ChatCord Bot';
 // Run when client connects
 io.on('connection', (socket) => {
   socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
     // Welcome current user
     socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     // Broadcast when a user connects
-    socket.broadcast.emit(
-      'message',
-      formatMessage(botName, 'A user has joined the chat')
-    );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `A ${user.username} has joined the chat`)
+      );
   });
 
   // Listen for chatMessage
